2070d2a747176b6eb67e76a151d4066263460ba5
jose miguel mut <jomutlo@gmail.com>
2023-04-30 16:34:01 +0100

    you can steer a bird

--- src/bird.rs
+++ src/bird.rs
@@ -3,6 +3,7 @@ use macroquad::prelude::Vec2;
 pub struct Bird {
     pos: Vec2,
     dir: Vec2,
+    speed: f32,
 }
 
 pub struct BirdTriangle {
@@ -13,13 +14,36 @@ pub struct BirdTriangle {
 
 impl Bird {
     pub fn new(pos: Vec2, dir: Vec2) -> Self {
-        Self { pos, dir }
+        Self { pos, dir, speed: dir.dot(dir).sqrt(), }
     }
+
     pub fn get_triangle(&self) -> BirdTriangle {
         BirdTriangle {
             front: self.pos + self.dir,
             left: self.pos - self.dir*0.5 + rotate_left(self.dir*0.5),
             right: self.pos - self.dir*0.5 + rotate_right(self.dir*0.5),
+        }
+    }
+
+    pub fn rotate(&mut self, angle_in_radians: f32) {
+        let new_unnormalized_dir = rotate_angle(self.dir, angle_in_radians);
+        self.dir = new_unnormalized_dir.normalize() * self.speed;
+    }
+
+    pub fn advance_toroid(&mut self, width: f32, height: f32) {
+        self.pos += self.dir;
+        // assumes that increments are smaller than 1 whole screen
+        if self.pos.x < 0.0 {
+            self.pos.x += width;
+        }
+        if self.pos.x >= width {
+            self.pos.x -= width;
+        }
+        if self.pos.y < 0.0 {
+            self.pos.y += height;
+        }
+        if self.pos.y >= height {
+            self.pos.y -= height;
         }
     }
 }
@@ -31,8 +55,15 @@ fn rotate_right(direction: Vec2) -> Vec2 {
     Vec2::new(-direction.y, direction.x)
 }
 
+// google rotation matrices to understand this
+fn rotate_angle(direction: Vec2, angle_in_radians: f32) -> Vec2 {
+    Vec2::new(angle_in_radians.cos() * direction.x - angle_in_radians.sin() * direction.y,
+              angle_in_radians.sin() * direction.x + angle_in_radians.cos() * direction.y)
+}
+
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::PI;
     use super::*;
 
     #[test]
@@ -47,5 +78,37 @@ mod tests {
         let right_down = Vec2::new(0.5, 0.1);
         let down_left = Vec2::new(-0.1, 0.5);
         assert_eq!(rotate_right(right_down), down_left);
+    }
+
+    fn assert_float_eq(a: f32, b: f32) {
+        const EPSILON:f32 = 0.0001;
+        if (a - b).abs() > EPSILON {
+            panic!("floats were not approximately equal!\n  {}\n  {}", a, b);
+        }
+    }
+
+    fn assert_vec2_eq(a: Vec2, b: Vec2) {
+        assert_float_eq(a.x, b.x);
+        assert_float_eq(a.y, b.y);
+    }
+
+    #[test]
+    fn test_rotate_angle() {
+        let right_down = Vec2::new(0.5, 0.1);
+        let down_left = Vec2::new(-0.1, 0.5);
+        let rotated = rotate_angle(right_down, PI * 0.5);
+        assert_vec2_eq(rotated, down_left);
+        let rotated = rotate_angle(right_down, PI * 0.4);
+        assert_vec2_eq(rotated, Vec2::new(0.0594, 0.50643));
+    }
+
+    #[test]
+    fn test_rotate_bird() {
+        let mut bird = Bird::new(Vec2::default(), Vec2::new(0.5, 0.1));
+        let front = bird.get_triangle().front;
+        bird.rotate(PI * 0.5);
+        let rotated_front = bird.get_triangle().front;
+        let down_left = Vec2::new(-0.1, 0.5);
+        assert_vec2_eq(rotated_front, down_left);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+
 mod bird;
 
+use std::f32::consts::PI;
 use macroquad::prelude::*;
 use macroquad::ui::root_ui;
 use macroquad::ui::widgets::Label;
@@ -8,14 +10,16 @@ use crate::bird::{Bird, BirdTriangle};
 const DEFAULT_WINDOW_TITLE: &'static str = "Boids";
 const DEFAULT_WINDOW_WIDTH: i32 = 256*4;
 const DEFAULT_WINDOW_HEIGHT: i32 = 256*3;
+const ANGULAR_SPEED: f32 = PI * 0.02; // in radians per frame
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let bird = Bird::new(Vec2::new(screen_width()*0.5, screen_height()*0.5), Vec2::new(10.0, 0.0));
+    let mut bird = Bird::new(Vec2::new(screen_width()*0.5, screen_height()*0.5), Vec2::new(10.0, 0.0));
     loop {
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
+        control_bird(&mut bird);
         clear_background(LIGHTGRAY);
         draw_bird(&bird);
         next_frame().await
@@ -30,7 +34,15 @@ fn window_conf() -> Conf {
         ..Default::default()
     }
 }
-
+fn control_bird(bird: &mut Bird) {
+    if is_key_down(KeyCode::Left) {
+        bird.rotate(-ANGULAR_SPEED);
+    }
+    if is_key_down(KeyCode::Right) {
+        bird.rotate(ANGULAR_SPEED);
+    }
+    bird.advance_toroid(screen_width(), screen_height());
+}
 fn draw_bird(bird: &Bird) {
     let BirdTriangle { front, left, right } = bird.get_triangle();
     draw_triangle(front, left, right, DARKGREEN);
